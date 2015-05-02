f790bb5a4fa0b4ea4664ff9baa0b7677dbbf851e
ankit182 <1994ankitagrawal@gmail.com>
2015-05-02 16:24:16 +0530

    rds

--- wactor/settings.py
+++ wactor/settings.py
@@ -88,11 +88,14 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
